2ecbcc14b15ca0d32e0c0751f519a13aa23e4443
wartgowj <wartgowj@gmail.com>
2018-01-22 20:40:15 -0800

    twitter functional

--- liri.js
+++ liri.js
@@ -1,11 +1,11 @@
 const dotenv = require('dotenv').config();
-//const twitter = require('twitter');
-const Spotify = require('node-spotify-api');
+
+
 //const request = require('request');
 const keys = require('./keys.js');
 
-const spotify = new Spotify(keys.spotify);
-//const client = new Twitter(keys.twitter);
+
+
 
 const command = process.argv[2];
 const search = process.argv[3];
@@ -30,22 +30,40 @@ switch(command) {
 
 
 //'my-tweets' function
+function getTweets(){
+	const Twitter = require('twitter');
+	const client = new Twitter(keys.twitter);
+	var params = {screen_name: 'wartgowj'};
+	client.get('statuses/user_timeline', params, function(error, tweets, response) {
+  	if (!error) {
+  		for (i = 0; i < tweets.length; i++){
+      	console.log(tweets[i].text);
+      	console.log(tweets[i].created_at);
+      	console.log("-------------------------------------------------");
+ 	 }
+  	} else {
+      throw error
+  }
+});
+}
 
 //'spotify-this-song' function
 function getSong(){
-	spotify.search({ type: 'track', query: search, limit:1 }, function(err, data) {
+	const Spotify = require('node-spotify-api');
+	const spotify = new Spotify(keys.spotify);
+	spotify.search({ type: 'track', query: search, limit:5 }, function(err, data) {
   		if (err) {
     		return console.log('Error occurred: ' + err);
   }
  
-	console.log(JSON.stringify(data.tracks.items[0].artists[0].name, null, 2));
-	console.log(JSON.stringify(data.tracks.items[0].name, null, 2));
+	console.log(JSON.stringify("Artist:" + " " + data.tracks.items[0].artists[0].name, null, 2));
+	console.log(JSON.stringify("Title:" + " " + data.tracks.items[0].name, null, 2));
 	if(data.tracks.items[0].preview_url){
-	console.log(JSON.stringify(data.tracks.items[0].preview_url, null, 2));
+	console.log(JSON.stringify("Preview:" + " " + data.tracks.items[0].preview_url, null, 2));
 	}else{
 		console.log("Sorry, No Preview Available")
 	}
-	console.log(JSON.stringify(data.tracks.items[0].album.name, null, 2));
+	console.log(JSON.stringify("Album:" + " " + data.tracks.items[0].album.name, null, 2));
 	});
 }	
 
