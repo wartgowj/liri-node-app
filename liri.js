8416cdda99841849a92b9dc59fecb4f08b6aeb14
wartgowj <wartgowj@gmail.com>
2018-01-22 17:50:34 -0800

    made first files

---
+++ liri.js
@@ -0,0 +1,13 @@
+const dotenv = require('dotenv').config();
+
+const spotify = new Spotify(keys.spotify);
+const client = new Twitter(keys.twitter);
+
+
+//'my-tweets' command
+
+//'spotify-this-song' command
+
+//'movie-this' command
+
+//'do-what-it-says' command
